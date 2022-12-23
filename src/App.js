34c80fc1644807cf5ca28de19fe5157d79530f5c
Md Mahbub Alam Khan <md.m.khan@tuni.fi>
2022-12-24 01:12:28 +0200

    - Updated navigation responsive issue.

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import "./styles/main.scss"
 
 // Load all the JS modules.
 import Menu from "./scripts/modules/Menu"
-import Products from "./scripts/modules/Products"
 
 // Call All JS Classes.
 new Menu()
-new Products()
